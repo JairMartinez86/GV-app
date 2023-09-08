6642bed1cc8fe378d7398e52a88f2497b044d085
JairMartinez86 <jmartinezg86@gmail.com>
2023-09-07 21:13:04 -0600

    login

--- src/app/SHARED/componente/login/login.component.ts
+++ src/app/SHARED/componente/login/login.component.ts
@@ -48,8 +48,10 @@ export class LoginComponent {
   }
 
   private v_Limpiar() {
-    this.val.Get("txtUsuario")?.setValue("OFONSECA");
-    this.val.Get("txtPass")?.setValue("1987");
+    //this.val.Get("txtUsuario")?.setValue("OFONSECA");
+    //this.val.Get("txtPass")?.setValue("1987");
+    this.val.Get("txtUsuario")?.setValue("");
+    this.val.Get("txtPass")?.setValue("");
     this.val.Iniciar = true;
 
     this.DynamicFrom?.viewContainerRef.clear();
